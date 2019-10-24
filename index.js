9e31e5f850168876c5bfbd0e2eb3003812995630
Shadow Smith <shadow@businesscreditreports.com>
2019-10-24 10:47:15 -0500

    Refactor api code

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 var express = require('express')
 const path = require('path')
 const app = express()
+const domainPing = require('domain-ping')
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: false
+}))
 
-app.use('/', require('./routes'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.post('/', (req, res) => {
+
+  let url = req.body.url
+
+  const includesHttps = url.includes('https://')
+  const includesHttp = url.includes('http://')
+  const includesWww = url.includes('www.')
+
+  if (includesHttps) {
+    url = url.replace('https://', '')
+  } else if (includesHttp) {
+    url = url.replace('http://', '')
+  } else if (includesWww) {
+    url = url.replace('www.', '')
+  }
+
+  domainPing(url)
+    .then(response => {
+      res.send(response)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+
+})
 
 const PORT = process.env.PORT || 7000
 
